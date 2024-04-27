db9c891638e1b0f8e3f90cbe6111c97cec0abd75
Rajeev Sahu <rajeevsahu210@gmail.com>
2024-04-28 02:17:19 +0530

    added .env

--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2023_11_23_061426) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.datetime "deleted_at", precision: 6
+    t.datetime "deleted_at"
     t.text "images", default: [], array: true
     t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
